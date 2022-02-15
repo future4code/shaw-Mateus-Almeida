6ad79d3cf7a3dd1bc3207b205c782f1d0c7f5d57
Mateus Vinicius Almeida <98291052+MateusViniciusRs@users.noreply.github.com>
2022-02-15 20:52:51 -0300

    Update index.js

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -67,8 +67,8 @@ let numeroUm = Number(prompt("fale um numero!"))
     let numeroDois = Number(prompt("digite  outro numeoro!"))
         console.log("O primeiro numero é maior que segundo" , numeroUm > numeroDois) 
             console.log("O primeiro numero é igual ao segundo", numeroUm === numeroDois)
-        console.log("O primeiro numero é divisível pelo segundo?" , (numeroUm % numeroDois)  === 0)
-    console.log('O segundo numero é divisível pelo primeiro? ' , (numeroDois % numeroUm) === 0)
+        console.log("O primeiro numero é divisível pelo segundo?" , (numeroUm !== numeroDois)  === 0)
+    console.log('O segundo numero é divisível pelo primeiro? ' , (numeroDois !== numeroUm) === 0)
     */
 
 
