29b8956b092caa6015e93ea1f331fd4d3b88a4eb
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-02-23 17:11:10 -0300

    lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,72 @@
+/*
+exercicio de interpretaçao de codigo!
+
+1: ele esta somando a repetiçao de i  e armazenando o valor de da volta entao o resultado e 10
+
+
+2: sera impreso todos os numero maiores que 18  que sao :,19, 21, 23, 25, 27, 30]
+b , acho que sim ! porem nao entendi muto bem a pergunta
+
+3 :vai imprimir os * na tela se digitar 4 sera impreso 4 linhas
+começando com 
+*
+**
+***
+****
+
+
+
+
+*/
+
+// exercicio 1
+/*
+let i = 0
+ let novoBicho =[]
+ let nomeBicho =[]
+ let numeroBicho =Number(prompt("voce tem algum pet!"))
+ if (numeroBicho === 0){
+ console.log("Que pena! Você pode adotar um pet!")
+}else {
+   while (i < numeroBicho) {
+    nome = prompt("me fale o nome do seu bicho ")
+    nomeBicho.push(nome)
+     i++
+  }
+}  
+  
+  console.log(nomeBicho)
+   */
+
+
+  
+  
+  
+   
+  
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
