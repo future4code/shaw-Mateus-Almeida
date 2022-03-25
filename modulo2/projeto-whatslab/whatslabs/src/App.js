6206aa8d2bb15384aa07c84b963958816c47e2cd
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-03-25 15:56:22 -0300

    projeto.0.3

--- modulo2/projeto-whatslab/whatslabs/src/App.js
+++ modulo2/projeto-whatslab/whatslabs/src/App.js
@@ -2,26 +2,53 @@
 import './App.css';
 import React from 'react';
 import { render } from '@testing-library/react';
+import styled from "styled-components";
+
+const MainDiv = styled.div`
+display: flex;
+justify-content: center;
+flex-direction:column;
+align-items: center;
 
 
 
+`
+const Borda = styled.div`
+width: 600px;
+height: 98vh;
+border: 4px solid black;
+background-color: rgba(104, 184, 157, 0.568);
+align-items:  end;
+display: flex;
 
-
+`
+/*
+const Enviarr = styled.div`
+display: flex;
+flex-direction: row;
+width; 100%;
+`
+*/
 
 function App(props) {
  
    
   return(
-  <div>
-    <input className='nome'/>
-  <input/>
-  <button>enviar </button>
+    <MainDiv>
+  <Borda >
+   
+    
+ 
+  <input className="nome"/>
+  <input className="a"/>
+  <button className="b">enviar </button>
 
 
 
 
-  </div>
-  
+  </Borda>
+
+  </MainDiv>
 
   )
 }
