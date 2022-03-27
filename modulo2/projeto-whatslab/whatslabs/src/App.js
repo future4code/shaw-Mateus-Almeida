2e0d04318e82d7dfab93ce47919c0fd771adbfe9
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-03-27 15:40:36 -0300

    fazendo-os-eventos

--- modulo2/projeto-whatslab/whatslabs/src/App.js
+++ modulo2/projeto-whatslab/whatslabs/src/App.js
@@ -3,23 +3,43 @@ import './App.css';
 import React from 'react';
 import { render } from '@testing-library/react';
 import styled from "styled-components";
+import styledComponents from 'styled-components';
+   
+const Paizao = styled.div`
+display: flex;
+align-itens: center;
+justify-content: center;
+box-sizing:border-box;
+text-align:center;
+
+`
+
+
+
+
 
 const MainDiv = styled.div`
 display: flex;
-justify-content: center;
-flex-direction:column;
-align-items: center;
-
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items:flex-start;
+  margin:0px;
+  width: 600px;
+  height:100vh;
+  border: 6px solid black;
+ 
 
 
 `
+// set state e o que eu crio ,setstate e para auterar o estado
 const Borda = styled.div`
-width: 600px;
-height: 98vh;
-border: 4px solid black;
-background-color: rgba(104, 184, 157, 0.568);
-align-items:  end;
 display: flex;
+flex-direction: column;
+justify-content: flex-end;
+align-items: flex-start;
+width: 580px;
+height: 100vh;
+padding: 20px;
 
 `
 /*
@@ -29,28 +49,102 @@ flex-direction: row;
 width; 100%;
 `
 */
+const Mensagen = styledComponents.div`
 
-function App(props) {
- 
+display: flex;
+flex-direction: column;
+justify-content:center;
+margin-bottom: 28px;
+
+
+
+`
+ const  Nomeu = styledComponents.input`
+ width: 12%;
+  border: 4%;
+  border: 3px solid black;
+  
+  margin-bottom: -16px;
+  font-weight:bold;
+ `
+const Mens = styledComponents.input`
+width: 60%;
+    border: 20%;
+    border: 3px solid black;
+    left:  0px;
+    margin-bottom: -16px;
    
+  
+`
+const Enviarr = styledComponents.button`
+border: 8%;
+border: 3px solid black;
+left:  0px;
+margin-bottom: -16px;
+
+`
+const BoxInput = styledComponents.div`
+display:flex;
+margin:0px;
+backgroundcolor: rgb(153, 2, 2);
+
+`
+
+
+/*function App(props) { */
+class App extends React.Component {
+ state ={
+    controleMensagem:"",
+    controleNomeu:""
+
+ }
+ onChangeNomeu=(event)=>{
+   this.setState({controleNomeu: event.target.value})
+   console.log(this.state.controleNomeu)
+ }
+   onChangeMensagem =(event)=>{
+    this.setState({controleMensagem: event.target.value})
+    console.log(this.state.controleMensagem)
+  
+  
+  } 
+  onClikButton =()=>{
+    this.setState({controleNomeu:""})
+    this.setState({controleMensagem:""})
+  }
+
+ 
+
+
+
+render(){
+
+
+
+
+
+
   return(
+    <Paizao>
     <MainDiv>
+       
   <Borda >
-   
-    
- 
-  <input className="nome"/>
-  <input className="a"/>
-  <button className="b">enviar </button>
-
-
-
-
+  <Mensagen> DSFASDFADSFASD</Mensagen>
+  <BoxInput>
+  <Nomeu onChange={this.onChangeNomeu}
+  value={this.state.controleNomeu}  
+  placeholder='nome'/>
+  <Mens onChange={this.onChangeMensagem}
+  value={this.state.controleMensagem}  placeholder='mensagem'/>
+  <Enviarr  onClick={this.onClikButton} type='submit'>enviar </Enviarr>
+  </BoxInput>
   </Borda>
-
   </MainDiv>
-
+  </Paizao>
   )
+  }
 }
+
+
 
 export default App;
