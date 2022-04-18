cfb2b9774fbc0c05495466d6c6afdda3aeb7c63a
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-04-18 16:08:57 -0300

    inicial

---
+++ modulo-3/useState/insta-lab-hooks/insta-lab-hooks/src/components/Post/Post.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { PostContainer, PostHeader, UserPhoto, PostPhoto, PostFooter, CommentContainer } from './styles'
+
+import IconeComContador from '../IconeComContador/IconeComContador'
+import SecaoComentario from '../SecaoComentario/SecaoComentario'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+
+const Post = (props) => {
+  const [curtido, setCurtido] = useState(false)
+  const [NumeroCurtidas, setNumeroCurtidas] = useState(0)
+  const [comentando, setComentando] = useState()           // queria usar um []
+  const [numeroComentario, setNumeroComentario] = useState(0)
+  const [comentarios, setcomentarios] = useState()         // queria usar um []
+  
+  
+
+
+  const onClickCurtida = () => {
+   setCurtido(curtido ? false : true)
+   
+  };
+
+  const onClickComentario = () => {
+  };
+
+  const enviarComentario = (comentario) => {
+  }
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          // icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          // valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          // valorContador={numeroComentarios}
+        />
+      </PostFooter>
+      {/* {caixaDeComentario} */}
+    </PostContainer>
+  )
+}
+
+export default Post
