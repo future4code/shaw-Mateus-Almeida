e53b33740c3048f0bc2d691429a1d12f2220a5b8
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-06-01 15:53:33 -0300

    feito

---
+++ modulo-5/fluxo-de-dados-backend/template-exercicio/src/data.ts
@@ -0,0 +1,30 @@
+
+type pessoa = {
+   id: string,
+   name: String,
+   price: Number
+}
+
+
+
+const produtos: pessoa[] = [
+
+   {
+      id: "1",
+      name: "carro",
+      price: 1500000
+   },
+   {
+      id: "2",
+      name: "motohome",
+      price: 90000
+
+   },
+   {
+      id: "3",
+      name: "bike",
+      price: 1200
+
+   }
+]
+export default produtos
