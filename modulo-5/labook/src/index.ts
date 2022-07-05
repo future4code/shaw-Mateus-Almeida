fe6b16a1aab9c1e381b73aa309219470315730a7
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-07-05 14:03:59 -0300

    labook

---
+++ modulo-5/labook/src/index.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import express ,{Express} from 'express'
+import cors from 'cors'
+import {AddressInfo} from 'net'
+
+
+dotenv.config();
+const app: Express = express();
+app.use(express.json())
+app.use(cors())
+// servidor rodando 
+const server =app.listen(3003,()=>{
+    if(server){
+const address = server.address() as AddressInfo;
+console.log("servido rodando na porta 3003");
+
+    }else{
+        console.log("servidor não esta rodando ");
+        
+    }
+})
