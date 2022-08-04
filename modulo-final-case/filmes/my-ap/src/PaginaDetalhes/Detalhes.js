eed7dab7cb0f3ff809a603249fd95d2204c228e3
Mateus Vinicius Almeida <mateusviniciusnunu@gmail.com>
2022-08-04 16:05:19 -0300

    versao-inicial

---
+++ modulo-final-case/filmes/my-ap/src/PaginaDetalhes/Detalhes.js
@@ -0,0 +1,16 @@
+
+
+// CHAVE API 0b7665ca85a18198756afe296597bf9e
+
+
+import React from "react"
+
+ const Detalhes = () => {
+
+    return(
+        <div>
+            detalhes
+        </div>
+    )
+}
+export default Detalhes
---
+++ modulo-final-case/filmes/my-ap/src/PaginaPrincipal/styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+
+export const DivPrincipal =styled.div`
+display: flex;
+flex-direction: column;
+background-color: rgb(45, 12, 93);
+height: 45%;
+
+
+h1{
+    color: white;
+    margin-left: 12%;
+}
+p{
+    color: white;
+    margin-left: 44%;
+   
+}
+`
+export const DivGeneros =styled.button`
+
+background-color:  white;
+color: black;
+
+flex-direction: row;
+justify-content:center;
+align-items: center;
+width: 10%;
+height: 20%;
+border-radius: 18%;
+margin-top: 1%;
+
+margin-left: 4%;
+
+`
